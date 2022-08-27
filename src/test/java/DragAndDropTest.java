aae7f9a90153446489e7d442dde16d0dc1fafda3
Dinar <gizzyatovdinar@gmail.com>
2022-08-27 14:43:43 +0400

    add tests

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class DragAndDropTest {
+
+    @Test
+    public void testCheckDragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+//        actions().moveToElement($("#column-a")).clickAndHold()
+//            .moveToElement($("#column-b")).release().perform();
+//        $("#column-b").hover();
+        assertEquals("A", $("#column-b").getText());
+        assertEquals("B", $("#column-a").getText());
+    }
 }
--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -1,2 +1,19 @@
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class GitHubTest {
+
+    @Test
+    public void checkPricingDownloadTest() {
+        open("https://github.com/");
+        $(byTagAndText("summary", "Pricing")).hover();
+        $(byText("Compare plans")).click();
+        $(".h2-mktg").shouldHave(text("Get the complete developer platform."));
+    }
 }
