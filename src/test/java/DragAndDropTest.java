23529d1a3ee0f7c51b15cdf00f23eac57f6f51a5
Dinar <gizzyatovdinar@gmail.com>
2022-08-29 13:31:34 +0400

    test changed
    Assertions replaced by shouldHave

--- src/test/java/DragAndDropTest.java
+++ src/test/java/DragAndDropTest.java
@@ -1,4 +1,3 @@
-
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
@@ -10,9 +9,9 @@ public class DragAndDropTest {
     public void testCheckDragAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo("#column-b");
-//        actions().moveToElement($("#column-a")).clickAndHold()
-//            .moveToElement($("#column-b")).release().perform();
-//        $("#column-b").hover();
+        actions().moveToElement($("#column-a")).clickAndHold()
+            .moveToElement($("#column-b")).release().perform();
+        $("#column-b").hover();
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
     }
